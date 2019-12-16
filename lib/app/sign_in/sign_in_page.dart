b74d1a00c975514ae8befe85ea293cb227081d40
Tiger <thisisstreamliner@gmail.com>
2019-12-16 15:38:16 +0200

    adding column

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -6,6 +6,30 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            child: SizedBox(height: 100.0,),
+          ),
+          SizedBox(height: 8,),
+          Container(
+            child: SizedBox(height: 100.0,),
+          ),
+          SizedBox(height: 8,),
+          Container(
+            child: SizedBox(height: 100.0,),
+          ),
+        ],
       ),
     );
   }
