401e0426687923c022f41a121fb3dba77c926716
Tiger <thisisstreamliner@gmail.com>
2019-12-17 12:42:49 +0200

    setting default values for button

--- lib/app/widgets/custom_raised_button.dart
+++ lib/app/widgets/custom_raised_button.dart
@@ -5,18 +5,28 @@ class CustomRaisedButton extends StatelessWidget {
   final VoidCallback onPressed;
   final Widget child;
   final double borderRadius;
+  final double height;
   final Color color;
 
-  CustomRaisedButton({@required this.child, @required this.onPressed, this.color, this.borderRadius});
+  CustomRaisedButton({
+    @required this.child, 
+    @required this.onPressed, 
+    this.color, 
+    this.height : 50.0 ,
+    this.borderRadius : 2.0
+  });
 
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: child,
-      onPressed: onPressed,
-      color: color,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(borderRadius))
+    return SizedBox(
+      height: height,
+        child: RaisedButton(
+        child: child,
+        onPressed: onPressed,
+        color: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(borderRadius))
+        ),
       ),
     );
   }
