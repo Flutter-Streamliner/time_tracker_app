b8b6a005d48bcccfe671a5479557d183a31ffd5e
Tiger <thisisstreamliner@gmail.com>
2020-01-28 12:24:38 +0200

    Defining a common API path class

--- lib/app/services/database.dart
+++ lib/app/services/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
+import 'package:time_tracker_app/app/services/api_path.dart';
 
 abstract class Database {
 
@@ -15,7 +16,7 @@ class FirestoreDatabase implements Database {
   FirestoreDatabase({@required this.uid}) : assert(uid != null);
 
   Future<void> createJob(Job job) async {
-    final path = '/users/$uid/jobs/job_123';
+    final path = APIPath.job(uid, 'job_abc'); //'/users/$uid/jobs/job_123';
     final documentReference = Firestore.instance.document(path);
     await documentReference.setData(job.toMap());
   }
