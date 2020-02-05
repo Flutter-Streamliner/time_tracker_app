59240fe9405b6c4788befdf84e45fd5d7264e72b
Tiger <thisisstreamliner@gmail.com>
2020-02-05 16:48:15 +0200

    Validating and saving form data

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -16,16 +16,44 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+
+  final _formKey = GlobalKey<FormState>();
+  String _name;
+  int _ratePerHour;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
         title: Text('New Job'),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Save', style: TextStyle(fontSize: 18, color: Colors.white)),
+            onPressed: _submit,
+          ),
+        ],
       ),
       body: _buildContents(),
       backgroundColor: Colors.grey[200],
     );
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('form saved name: $_name ratePerHour: $_ratePerHour');
+    }
+    // TODO: submit data to Firestore
+    
+  }
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
   }
 
   Widget _buildContents() {
@@ -44,6 +72,7 @@ class _AddJobPageState extends State<AddJobPage> {
 
   Widget _buildForm() {
     return Form(
+      key: _formKey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: _buildFormChildren(),
@@ -55,10 +84,13 @@ class _AddJobPageState extends State<AddJobPage> {
     return [
       TextFormField(
         decoration: InputDecoration(labelText: 'Job name'),
+        validator: (value) => value.isNotEmpty ? null : 'Name can\'t be empty',
+        onSaved: (value) => _name = value,
       ),
       TextFormField(
         keyboardType: TextInputType.numberWithOptions(signed: false, decimal: false),
         decoration: InputDecoration(labelText: 'Rate per hour'),
+        onSaved: (value) => _ratePerHour = int.tryParse(value) ?? 0,
       ),
     ];
   }
