dc9ad32fac3452630de4791d2da3ec7776f56599
Tiger <thisisstreamliner@gmail.com>
2020-02-18 10:29:07 +0200

    enforcing unique job names

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/app/services/database.dart';
+import 'package:time_tracker_app/app/widgets/platform_alert_dialog.dart';
 import 'package:time_tracker_app/app/widgets/platform_exception_alert_dialog.dart';
 
 class AddJobPage extends StatefulWidget {
@@ -52,9 +53,19 @@ class _AddJobPageState extends State<AddJobPage> {
   Future<void>  _submit() async {
     if (_validateAndSaveForm()) {
       try {
-        final job = Job(name: _name, ratePerHour: _ratePerHour);
-        await widget.database.createJob(job); 
-        Navigator.of(context).pop();
+        final jobs = await widget.database.jobsStream().first;
+        final allNames = jobs.map((job) => job.name).toList();
+        if (allNames.contains(_name)) {
+          PlatformAlertDialog(
+            title: 'Name already used',
+            content: 'Please choose a different job name',
+            defaultActionText: 'OK',
+          ).show(context);
+        } else {
+          final job = Job(name: _name, ratePerHour: _ratePerHour);
+          await widget.database.createJob(job); 
+          Navigator.of(context).pop();
+        }
       } on PlatformException catch(e) {
         PlatformExceptionAlertDialog(
           title: 'Operation failed',
