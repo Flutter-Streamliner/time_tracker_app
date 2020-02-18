41ee40708de991b08902f98722c4070c237ae826
Tiger <thisisstreamliner@gmail.com>
2020-02-18 10:41:29 +0200

    Adding a custom JobListTile

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:time_tracker_app/app/home/jobs/add_job_page.dart';
+import 'package:time_tracker_app/app/home/jobs/job_list_tile.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/app/services/auth.dart';
 import 'package:time_tracker_app/app/services/database.dart';
@@ -39,7 +40,10 @@ class JobPage extends StatelessWidget {
        builder: (context, snapshot) {
           if (snapshot.hasData) {
             final jobs = snapshot.data;
-            final children = jobs.map((job) => Text(job.name)).toList( );
+            final children = jobs.map((job) => JobListTile(
+                                                job: job,
+                                                onTap: (){},
+                                              ),).toList( );
             return ListView(children: children,);
           }
           if (snapshot.hasError) {
