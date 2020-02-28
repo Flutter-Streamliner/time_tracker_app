b07467da56b341eb303ccbc698df0e56cc155400
Tiger <thisisstreamliner@gmail.com>
2020-02-28 12:15:07 +0200

    Add widget click testing logic

--- test/custom_raised_button_test.dart
+++ test/custom_raised_button_test.dart
@@ -3,15 +3,19 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:time_tracker_app/app/widgets/custom_raised_button.dart';
 
 void main() {
-  testWidgets('', (WidgetTester tester) async {
+  testWidgets('onPressed callback', (WidgetTester tester) async {
+    var pressed = false;
     final app = MaterialApp(home: CustomRaisedButton(
       child: Text('Tap me'),
+      onPressed: () => pressed = true,
     ));
     await tester.pumpWidget(app);
     final button = find.byType(RaisedButton);
     expect(button, findsOneWidget);
     expect(find.byType(FlatButton), findsNothing);
     expect(find.text('Tap me'), findsOneWidget);
+    await tester.tap(button);
+    expect(pressed, true);
   });
 
 }
